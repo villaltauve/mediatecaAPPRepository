c72c7d509bbdc8e45f892604ff12eb2b4261732c
villaltauve <villaltauve3@gmail.com>
2025-05-26 14:37:46 -0600

    first app commit

--- mediateca_v01/src/mediateca_v01/DatabaseConnection.java
+++ mediateca_v01/src/mediateca_v01/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://database-1.cpecymc4ejum.us-east-1.rds.amazonaws.com:3306/mediateca";
-    private static final String USER = "user90";
-    private static final String PASSWORD = "rdb01awspass";
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
